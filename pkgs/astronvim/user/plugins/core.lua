d7eeb9d86ff650e34f199637846f6cafa5b30c2b
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-11-06 13:41:06 +0100

    feat: delete unnecessary customizations that I haven't changed anything

--- pkgs/astronvim/user/plugins/core.lua
+++ pkgs/astronvim/user/plugins/core.lua
@@ -1,25 +1,4 @@
 return {
-  -- customize alpha options
-  {
-    "goolord/alpha-nvim",
-    opts = function(_, opts)
-      -- customize the dashboard header
-      opts.section.header.val = {
-        " █████  ███████ ████████ ██████   ██████",
-        "██   ██ ██         ██    ██   ██ ██    ██",
-        "███████ ███████    ██    ██████  ██    ██",
-        "██   ██      ██    ██    ██   ██ ██    ██",
-        "██   ██ ███████    ██    ██   ██  ██████",
-        " ",
-        "    ███    ██ ██    ██ ██ ███    ███",
-        "    ████   ██ ██    ██ ██ ████  ████",
-        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-        "    ██   ████   ████   ██ ██      ██",
-      }
-      return opts
-    end,
-  },
   -- customize gitsigns options
   {
     "gitsigns.nvim",
@@ -37,6 +16,4 @@ return {
   },
   -- override make command build since we provide the shared library with home-manager already
   { "nvim-telescope/telescope-fzf-native.nvim", build = "true", },
-  -- disable building since it's optional
-  { "L3MON4D3/LuaSnip", build = "true", },
 }
