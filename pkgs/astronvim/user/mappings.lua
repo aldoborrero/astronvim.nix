d408862151777278979e346210311c09ae10ff82
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-12-01 14:01:41 +0100

    chore: fmt

--- pkgs/astronvim/user/mappings.lua
+++ pkgs/astronvim/user/mappings.lua
@@ -3,42 +3,50 @@
 -- Please use this mappings table to set keyboard mapping since this is the
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
+
+local utils = require "astronvim.utils"
+
 return {
-  -- normal mode
-  n = {
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
+	-- normal mode
+	n = {
+		-- tables with the `name` key will be registered with which-key if it's installed
+		-- this is useful for naming menus
+		["<leader>b"] = { name = "Buffers" },
 
-    -- close buffer with picker
-    ["<leader>bx"] = {
-      function()
-        require("astronvim.utils.status").heirline.buffer_picker(
-          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
-        )
-      end,
-      desc = "Pick buffer to close",
-    },
+		["<leader>bx"] = {
+			function()
+				require("astronvim.utils.status").heirline.buffer_picker(
+					function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+				)
+			end,
+			desc = "Pick buffer to close",
+		},
 
-    -- open file picker
-    ["<leader><leader>"] = {
-      function() require("telescope.builtin").find_files() end,
-      desc = "Find files",
-    },
+		-- open file picker
+		["<leader><leader>"] = {
+			function() require("telescope.builtin").find_files() end,
+			desc = "Find Files",
+		},
 
-    -- open emoji icons
-    ["<leader>i"] = {
-      "<cmd>IconPickerInsert<cr>",
-      desc = "Open emoji icon picker",
-    },
-  },
-  -- terminal mode
-  t = {
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
-  },
-  -- ex commands
-  c = {
-    ["w!!"] = { "w !sudo tee > /dev/null %", desc = "Write as sudo" },
-  },
+		-- open emoji icons
+		["<leader>i"] = {
+			"<cmd>IconPickerInsert<cr>",
+			desc = "Open Icon Picker",
+		},
+
+		-- open LLM repl
+		["<leader>tl"] = {
+			function() utils.toggle_term_cmd "llm repl" end,
+			desc = "Open LLM REPL",
+		},
+	},
+	-- terminal mode
+	t = {
+		-- setting a mapping to false will disable it
+		-- ["<esc>"] = false,
+	},
+	-- ex commands
+	c = {
+		["w!!"] = { "w !sudo tee > /dev/null %", desc = "Write as sudo" },
+	},
 }
