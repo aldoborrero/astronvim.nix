6a9c79a73966f4440ae9382f7f7d9a797871a909
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2024-07-06 00:24:11 +0200

    feat: improve script to invoke astronvim

--- pkgs/astronvim4/default.nix
+++ pkgs/astronvim4/default.nix
@@ -8,30 +8,59 @@
 }:
 writeShellScriptBin "astronvim4" ''
   set -efu
-
   unset VIMINIT
+
+  # Parse command line arguments
+  NVIM_APPNAME="${nvim-appname}"
+  NO_LINK=0
+  ARGS=()
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      --app-name)
+        NVIM_APPNAME="$2"
+        shift 2
+        ;;
+      --no-link)
+        NO_LINK=1
+        shift
+        ;;
+      *)
+        ARGS+=("$1")
+        shift
+        ;;
+    esac
+  done
+
   export PATH=${buildEnv {
     name = "lsp-servers";
     paths = astronvim4-config.lspToolingPkgs;
   }}/bin:$PATH
-  export NVIM_APPNAME=${nvim-appname}
+  export NVIM_APPNAME
 
   # Ensure XDG folders
   XDG_CONFIG_HOME=''${XDG_CONFIG_HOME:-$HOME/.config}
   XDG_DATA_HOME=''${XDG_DATA_HOME:-$HOME/.local/share}
   mkdir -p "$XDG_CONFIG_HOME" "$XDG_DATA_HOME"
-
   NVIM_CONFIG="$XDG_CONFIG_HOME/$NVIM_APPNAME"
   NVIM_DATA="$XDG_DATA_HOME/$NVIM_APPNAME"
 
-  # Link custom config and install plugins
-  ln -sfT ${astronvim4-config} "$NVIM_CONFIG"
+  # Link or copy custom config based on --no-link option
+  if [ $NO_LINK -eq 1 ]; then
+    rm -rf "$NVIM_CONFIG"
+    cp -R ${astronvim4-config} "$NVIM_CONFIG"
+  else
+    ln -sfT ${astronvim4-config} "$NVIM_CONFIG"
+  fi
+
+  # Install plugins and necessary tooling
   ${neovim}/bin/nvim --headless -c 'quitall'
 
+  # Link telescope-fzf-native plugin
   if [[ -d $NVIM_DATA/lazy/telescope-fzf-native.nvim ]]; then
     mkdir -p "$NVIM_DATA/lazy/telescope-fzf-native.nvim/build"
     ln -sf "${vimPlugins.telescope-fzf-native-nvim}/build/libfzf.so" "$NVIM_DATA/lazy/telescope-fzf-native.nvim/build/libfzf.so"
   fi
 
-  exec ${neovim}/bin/nvim "$@"
+  # Run nvim
+  exec ${neovim}/bin/nvim "''${ARGS[@]}"
 ''
