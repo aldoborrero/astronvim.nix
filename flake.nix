b1d80c75779fcd9e71c2508757724704ad6f97e6
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2024-07-21 20:08:38 +0200

    chore: update flake lock

--- flake.nix
+++ flake.nix
@@ -5,17 +5,20 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://numtide.cachix.org"
+      "https://cache.garnix.io"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
 
   inputs = {
     # nix packages
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    mynixpkgs.url = "github:aldoborrero/mynixpkgs";
 
     # nvim
     astronvim3 = {
@@ -87,6 +90,7 @@
             inherit system;
             nixpkgs = inputs.nixpkgs-unstable;
           };
+          myPkgs = inputs.mynixpkgs.packages.${system};
         };
 
         # devshell
