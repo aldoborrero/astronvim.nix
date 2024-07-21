c05489c3dbe8b13ce93da5b287ed977c0a21df50
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2024-07-21 20:07:15 +0200

    feat: load myPkgs to astro4 config

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,13 +2,14 @@
   perSystem = {
     pkgs,
     self',
+    myPkgs,
     ...
   }: {
     packages = rec {
       astronvim3-config = pkgs.callPackage ./astronvim3/config.nix {inherit inputs;};
       astronvim3 = pkgs.callPackage ./astronvim3 {inherit astronvim3-config;};
 
-      astronvim4-config = pkgs.callPackage ./astronvim4/config.nix {};
+      astronvim4-config = pkgs.callPackage ./astronvim4/config.nix {inherit myPkgs;};
       astronvim4 = pkgs.callPackage ./astronvim4 {inherit astronvim4-config;};
 
       astronvim = astronvim4;
